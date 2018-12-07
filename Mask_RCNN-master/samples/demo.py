84b5f08df2b04f79c622905de5dae5724469edf9
YourFigo <501385003@qq.com>
2018-12-07 13:16:25 +0800

    cnn

---
+++ Mask_RCNN-master/samples/demo.py
@@ -0,0 +1,103 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+print(ROOT_DIR)
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+# Import COCO config
+sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
+import coco
+
+%matplotlib inline 
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+print(COCO_MODEL_PATH)
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+print(IMAGE_DIR)
+
+
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+print(COCO_MODEL_PATH)
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+
+
+# Load a random image from the images folder
+# os.walk() 是一个generator，里面有三个元素root, dirs, files
+file_names = next(os.walk(IMAGE_DIR))[2]
+# image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+images = []
+for file_name in file_names:
+    images.append(skimage.io.imread(os.path.join(IMAGE_DIR,file_name)))
+#     print(skimage.io.imread(os.path.join(IMAGE_DIR,file_name)))
+
+# Run detection
+# results = model.detect([image], verbose=1)
+for image in images:
+    results = model.detect([image], verbose=1)
+
+# Visualize results
+# r = results[0]
+# visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+#                             class_names, r['scores'])
+for result in results:
+    r = result
+    visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                            class_names, r['scores'])
